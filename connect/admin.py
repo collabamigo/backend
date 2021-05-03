18278da7a08e349bbb3889a1d19d5b3d81147fb2
Heemank Verma <vheemank@gmail.com>
2021-05-03 21:06:17 +0530

    fix ultra max pro

--- connect/admin.py
+++ connect/admin.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 
-from django.contrib import admin
-from .models import React
-
 class connectCredentials(admin.ModelAdmin):
     list_display = ('_id', 'first_name', 'last_name','Age','Gender','Education','Email','Contact','handle','isvendor')
 
 
 class connectTeacher(admin.ModelAdmin):
     list_display = ('_id','Skill_set')
-    
+
 # Register your models here.
 
-admin.site.register(React, connectAdmin)
+admin.site.register(Credentials, connectCredentials)
+admin.site.register(Teacher, connectTeacher)
