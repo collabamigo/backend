88611d2dcbcf97482b808890c4ce4880e226256f
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-05-06 13:40:53 +0530

    Imported viewsets in connect/views.py

--- connect/views.py
+++ connect/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .serializer import *
 from .models import *
 
-# from rest_framework import viewsets
+from rest_framework import viewsets
 
 # from django.http import HttpResponse
 # from django.views.generic import View
