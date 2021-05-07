5c8aa08487bce597f5af5f0a0bed99f2daab9863
Heemank Verma <vheemank@gmail.com>
2021-05-07 12:19:20 +0530

    playing with id

--- connect/models.py
+++ connect/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 
 class Todo(models.Model):
+    id = models.AutoField(primary_key=True, unique=True,
+                          auto_created=True,
+                          serialize=False, verbose_name='ID')
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
@@ -14,8 +17,10 @@ class Todo(models.Model):
 
 
 class Profile(models.Model):
-    # id = models.CharField(primary_key=True, unique=True,
-    #   max_length = 30)
+    id = models.CharField(primary_key=True, unique=True,
+                          max_length=30,
+                          auto_created=False,
+                          serialize=False, verbose_name='ID')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(blank=True)
@@ -34,13 +39,14 @@ class Teacher(models.Model):
     id = models.CharField(primary_key=True, unique=True,
                           max_length=30, auto_created=False,
                           serialize=False, verbose_name='ID')
-    Skill_set = ArrayField(
-        models.CharField(max_length=30, blank=True), size=20)
+    Skill_set = ArrayField(ArrayField(
+        models.CharField(max_length=30, blank=True), size=2), size=5)
 
 
 class Skill(models.Model):
 
     id = models.CharField(primary_key=True, unique=True,
-                          max_length=30)
-    Teacher_set = ArrayField(
-        models.CharField(max_length=30, blank=True), size=20)
+                          max_length=30, auto_created=False,
+                          serialize=False, verbose_name='ID')
+    Teacher_set = ArrayField(ArrayField(
+        models.CharField(max_length=30, blank=True), size=2), size=5)
