b6334d6ddbe9587286b9d4fb821f487987a2ff1c
Heemank Verma <vheemank@gmail.com>
2021-05-08 21:36:18 +0530

    lol

--- connect/views.py
+++ connect/views.py
@@ -1,7 +1,7 @@
 from .models import Todo, Profile, Teacher, Skill
 from rest_framework import viewsets
 from rest_framework.views import APIView
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from .serializer import (TodoSerializer, ProfileSerializer,
                          TeacherSerializer, SkillSerializer,
                          TodoSerializer2)
