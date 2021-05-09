7f23945ca57076641a3b1c6ac6ff283a54102287
Heemank Verma <vheemank@gmail.com>
2021-05-09 16:19:25 +0530

    URL remapping and Email system implemetation

--- backend/settings.py
+++ backend/settings.py
@@ -42,8 +42,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ['EMAIL']
-EMAIL_HOST_PASSWORD = os.environ['PASS_KEY']
+EMAIL_HOST_USER = os.getenv['EMAIL']
+EMAIL_HOST_PASSWORD = os.getenv['PASS_KEY']
 
 
 # Application definition hai ye
