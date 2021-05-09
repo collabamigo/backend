efe959b1d56f52d1b37e9561d3a9e07eb3eb965a
Heemank Verma <vheemank@gmail.com>
2021-05-09 16:33:58 +0530

    URL remapping and Email system implemetation 4.0

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,7 @@ DEBUG = not bool(os.getenv("PRODUCTION"))
 
 ALLOWED_HOSTS = []
 
-# DataFlair
+# DataFlair neeche hai
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
