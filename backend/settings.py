c18e887aa27e4e9dd85b68d99c15fb097ffe4f2c
Heemank Verma <vheemank@gmail.com>
2021-05-11 13:08:39 +0530

    trying again 2.0

--- backend/settings.py
+++ backend/settings.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -147,7 +148,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# CSRF_COOKIE_SECURE = bool(os.getenv("PRODUCTION"))
+CSRF_COOKIE_SECURE = bool(os.getenv("PRODUCTION"))
 SECURE_SSL_REDIRECT = bool(os.getenv("PRODUCTION"))
 SESSION_COOKIE_SECURE = bool(os.getenv("PRODUCTION"))
 
