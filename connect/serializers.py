06858fe8a6040f70b3974d2f55677af817ae470f
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-05-21 19:29:53 +0530

    minor

--- connect/serializers.py
+++ connect/serializers.py
@@ -30,7 +30,7 @@ class TeacherSerializer(serializers.ModelSerializer):
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = ('id', 'Teacher_set')
+        fields = ('name', 'Teacher_set')
 
 
 class UserSerializer(serializers.ModelSerializer):
