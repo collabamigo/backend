c70a6eb6689dbb1bafafc5a60ff88f6165b64ca9
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-05-21 10:53:56 +0530

    python-dotenv

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,9 @@ from corsheaders.defaults import default_headers
 
 import dj_database_url
 import django_heroku
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
