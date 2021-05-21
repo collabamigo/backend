d9e34bd8ca5a8491c5ef68bc5642d71266f00151
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-05-21 22:55:55 +0530

    rating app implemented

--- rating/ratingHandler.py
+++ rating/ratingHandler.py
@@ -1,9 +1,35 @@
-# import os
-# import pymongo
+import os
+import pymongo
+
 DATABASE_NAME = 'rating'
-LOGS_COLLECTION_NAME = 'logs'
+COLLECTION_NAME = 'logs'
 
 
-def return_ratings(email, users):
+def return_ratings(student_email, users) -> dict:
+    db = pymongo.MongoClient(os.environ['MONGODB_URI'])[DATABASE_NAME]
+    collection = db[COLLECTION_NAME]
+    entry = collection.find_one({"_id": student_email})
+    final_entry = dict()
+    if users:
+        for i in users:
+            final_entry[i] = entry.get(i)
+    else:
+        final_entry = entry
+    return final_entry
+
+
+def set_ratings(student_email, teacher_email, vote) -> None:
+    db = pymongo.MongoClient(os.environ['MONGODB_URI'])[DATABASE_NAME]
+    collection = db[COLLECTION_NAME]
+    entry = collection.find_one({"_id": student_email})
+    if not entry:
+        entry = dict()
+    else:
+        collection.delete_one({"_id": student_email})
+    entry[teacher_email] = vote
+    collection.insert_one(entry)
+
+
+def worker():
+    # TODO: Create function using https://www.evanmiller.org/how-not-to-sort-by-average-rating.html
     pass
-    # TODO: Add function code
--- rating/views.py
+++ rating/views.py
@@ -1,3 +1,28 @@
-# from django.shortcuts import render
+import json
 
-# Create your views here.
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from connect.permissions import IsOwner
+from . import ratingHandler
+
+
+class Rating(APIView):
+    permission_classes = (
+        permissions.IsAuthenticated,
+        IsOwner, )
+
+    def get(self, request):
+        users = request.GET
+        if users:
+            users = json.loads(users)
+        return Response(ratingHandler.return_ratings(request.user.email, users))
+
+    def post(self, request):
+        if 'teacher' not in request.data or 'vote' not in request.data:
+            ratingHandler.set_ratings(request.user.email, request.data['teacher'], bool(int(request.data['vote'])))
+            return Response()
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
