0757a226ff1ff70bfd7331998202bfbe567a6bdb
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-05-23 17:29:02 +0530

    minor

--- connect/views.py
+++ connect/views.py
@@ -50,7 +50,7 @@ def profile_list(request):
 def detail(request, search):
     skill = Skill.objects.get(name=search)
     output = list(map(lambda item: str(item), skill.Teacher_set.all()))
-    return JsonResponse(output, safe=False)
+    return JsonResponse(output)
 
 
 def details(request):
@@ -64,7 +64,7 @@ def details(request):
         profileobject.update(teacherobject)
         output[j] = profileobject
         j += 1
-    return JsonResponse(output, safe=False)
+    return JsonResponse(output)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
