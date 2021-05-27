1e8974b74eee47303dfe3d0fdee8fefe519774ba
Heemank Verma <vheemank@gmail.com>
2021-05-27 16:02:58 +0530

    Signal Implementations 5

--- connect/apps.py
+++ connect/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_save, post_delete
 from django.utils.translation import ugettext_lazy as _
-from connect.signals import profile_isteacher_false, profile_isteacher_true
 
 
 class ConnectConfig(AppConfig):
@@ -10,5 +9,7 @@ class ConnectConfig(AppConfig):
 
     def ready(self):
         from connect.models import Teacher
+        from connect.signals import profile_isteacher_false, \
+            profile_isteacher_true
         post_save.connect(profile_isteacher_true, sender=Teacher)
         post_delete.connect(profile_isteacher_false, sender=Teacher)
