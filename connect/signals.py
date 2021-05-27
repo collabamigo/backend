86d5ea3cb92836380f27d9582e3d2995589969cd
Heemank Verma <vheemank@gmail.com>
2021-05-27 16:49:10 +0530

    Signal Implementations 9

--- connect/signals.py
+++ connect/signals.py
@@ -1,12 +1,22 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from connect.models import Profile, Teacher
+from connect.emailhandler import new_teacher_email
 
 
 @receiver(post_save, sender=Teacher)
 def profile_isteacher_true(sender, instance, created, **kwargs):
     if created:
         print(instance.id, flush=True)
+        b = Profile.objects.get(id=str(instance.id))
+        b.IsTeacher = True
+        person = {
+            "Id": b.id,
+            "Name": b.First_Name + " " + b.Last_Name,
+            "Email": str((b.email).email)
+            }
+        new_teacher_email(person)
+        b.save()
         Profile.objects.get(str(instance.id)).update(IsTeacher=True)
 
 
