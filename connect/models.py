9a76df4437f665f27edef6dfa758e23f217eae43
Heemank Verma <vheemank@gmail.com>
2021-05-28 14:18:10 +0530

    check2

--- connect/models.py
+++ connect/models.py
@@ -46,7 +46,7 @@ class Skill(models.Model):
                                          related_name='skills')
 
     def __str__(self):
-        return self.Teacher_set
+        return self.id
 
 
 class Teacher(models.Model):
