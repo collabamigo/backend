852628b1ac6f112e5e2daeac44ab4a76ab08ab4b
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-05-29 20:30:00 +0530

    minor

--- connect/views.py
+++ connect/views.py
@@ -170,7 +170,7 @@ class ConnectionApprove(views.APIView):
             if obj['approvedAt']:
                 return Response("Already approved",
                                 status=status.HTTP_208_ALREADY_REPORTED)
-            print(identifier + "valid request")
+            print(identifier + "valid request", flush=True)
             student = Profile.objects.get(id=obj['student'])
             teacher = Profile.objects.get(id=obj['teacher'])
             contact_details = {
@@ -190,7 +190,7 @@ class ConnectionApprove(views.APIView):
             for key in contact_details:
                 format_dict['contact'] += str(key) + ": " + \
                     contact_details[key] + "\n"
-            print(identifier+"Calling sendmail")
+            print(identifier+"Calling sendmail", flush=True)
             send_mail(to=[str(student.email.email)],
                       subject="CollabConnect Connection Request Approval",
                       body=email_templates.connection_approval_text.
