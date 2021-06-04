ff4ecf489886fafd69106c22d3a9073d4177ef6c
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-06-04 19:58:54 +0530

    SkillSet

--- connect/models.py
+++ connect/models.py
@@ -27,18 +27,6 @@ class Profile(models.Model):
         return self.id
 
 
-class Skill(models.Model):
-    name = models.CharField(primary_key=True, unique=True,
-                            max_length=30, auto_created=False,
-                            serialize=False, verbose_name='ID')
-    Teacher_set = models.ManyToManyField(to='connect.Teacher',
-                                         related_name='skills',
-                                         blank=True, )
-
-    def __str__(self):
-        return self.name
-
-
 class Teacher(models.Model):
     id = models.OneToOneField(to=Profile, related_name='teacher',
                               to_field='id', db_column='id',
@@ -67,3 +55,23 @@ class Teacher(models.Model):
             '-confidence',
             '-UpVotes',
             'DownVotes', ]
+
+
+class Skill(models.Model):
+    name = models.CharField(primary_key=True, unique=True,
+                            max_length=30, auto_created=False,
+                            serialize=False, verbose_name='ID')
+    Teacher_set = models.ManyToManyField(to='connect.Teacher',
+                                         related_name='skills',
+                                         blank=True, through='SkillSet')
+
+    def __str__(self):
+        return self.name
+
+
+class SkillSet(models.Model):
+    teacher = models.ForeignKey(Teacher,
+                                on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill,
+                              on_delete=models.CASCADE)
+    approvals = models.IntegerField(blank=False, default=0)
