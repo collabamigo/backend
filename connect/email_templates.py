d14dc249466125debc3bc11f13769b79b1a73504
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-06-26 20:05:45 +0530

    variable update

--- connect/email_templates.py
+++ connect/email_templates.py
@@ -773,10 +773,8 @@ Dear {receiverName},
 
 You have successfully become a contributor on CollabConnect !
 
-Every time someone asks for your help we'll be sharing your basic details :
-{personName},
-{personEmail},
-{personContact1},
+Every time someone asks for your help we'll be sharing your 
+basic details like your email address and Telegram handle.
 
 Also, check out the Dashboard section for more information.
 
