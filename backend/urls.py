f9d53b94b1bff6c933bd2af278ce8bc6f6acd929
Heemank Verma <vheemank@gmail.com>
2021-07-02 11:54:18 +0530

    profiling

--- backend/urls.py
+++ backend/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
+from django.conf import settings
+from django.urls import include, path
 
 from django.urls import path, include
 from django.contrib import admin
@@ -21,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('autocomplete/', include('autocomplete.urls')),
     path('connect/', include('connect.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
     path('rating/', include('rating.urls'))]
