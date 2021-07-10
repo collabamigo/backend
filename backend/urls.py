a423863285bfed60281e6bb2a75c5526f3e52bce
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-07-10 21:48:14 +0530

    flake

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
-from django.conf import settings
-from django.urls import include, path
 
 from django.urls import path, include
 from django.contrib import admin
