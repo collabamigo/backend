c8aed45e854a693c6ada8f949fdd693a055d27cc
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-08-06 11:22:20 +0530

    club model updates

--- club/models.py
+++ club/models.py
@@ -26,11 +26,44 @@ class Entries(models.Model):
     entries_id = models.ForeignKey(Competition.competition_id, on_delete=models.CASCADE)
     participant = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
+############################################################################################
+class Choices(models.Model):
+    choice = models.CharField(max_length=5000)
+    is_answer = models.BooleanField(default=False)
+
+
+class Questions(models.Model):
+    question = models.CharField(max_length= 10000)
+    question_type = models.CharField(max_length=20)
+    required = models.BooleanField(default= False)
+    answer_key = models.CharField(max_length = 5000, blank = True)
+    score = models.IntegerField(blank = True, default=0)
+    choices = models.ManyToManyField(Choices, related_name = "choices")
+
 
 class Form(models.Model):
     id = models.IntegerField(primary_key=True, unique=True,
                              max_length=6, auto_created=False,
                              serialize=False, verbose_name='ID')
     entries = models.ForeignKey(Entries.entries_id, on_delete=models.CASCADE)
+    edit_after_submit = models.BooleanField(default=False)
+    confirmation_message = models.CharField(max_length = 10000, default = "Your response has been recorded.")
+    is_quiz = models.BooleanField(default=False)
+    allow_view_score = models.BooleanField(default= True)
+    createdAt = models.DateTimeField(auto_now_add = True)
+    updatedAt = models.DateTimeField(auto_now = True)
+    collect_email = models.BooleanField(default=False)
+    questions = models.ManyToManyField(Questions, related_name = "questions")
 
 
+class Answer(models.Model):
+    answer = models.CharField(max_length=5000)
+    answer_to = models.ForeignKey(Questions, on_delete=models.CASCADE, related_name="answer_to")
+
+
+class Responses(models.Model):
+    response_code = models.CharField(max_length=20)
+    response_to = models.ForeignKey(Form, on_delete = models.CASCADE, related_name = "response_to")
+    responder_ip = models.CharField(max_length=30)
+    responder_email = models.EmailField(blank = True)
+    response = models.ManyToManyField(Answer, related_name = "response")
