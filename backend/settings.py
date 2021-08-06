2a79342f531f26eab0bc1db916ef607e22026e19
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-08-06 19:00:22 +0530

    temporary

--- backend/settings.py
+++ backend/settings.py
@@ -74,13 +74,11 @@ INSTALLED_APPS = [
     'connect.apps.ConnectConfig',
     'corsheaders',
     'autocomplete',
-    'debug_toolbar',
     'club',
 ]
 
 # TODO: Enable CSRF
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -161,23 +159,6 @@ INTERNAL_IPS = [
     # ...
     '127.0.0.1',
     # ...
-]
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.history.HistoryPanel',
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
 ]
 
 
