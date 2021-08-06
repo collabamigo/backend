648a6803862af64c40e835798b140d23cc7a6036
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-08-06 16:52:16 +0530

    club model updates

--- club/admin.py
+++ club/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Club, Competition, Entries, Choices, Form, Question,Answer, Response
+from .models import Club
+# Competition, Entries, Choices, Form, Question,
+#       Answer, Response
+
 
 # Register your models here.
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "link", "picture", "college", "join_date",)
+
+
+admin.site.register(Club, ClubAdmin)
