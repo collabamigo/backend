e72afb047a23b60eb20bd18ab59845ecc9650952
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-08-21 18:51:22 +0530

    Permission change

--- club/views.py
+++ club/views.py
@@ -1,5 +1,7 @@
 # from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+
+from connect.permissions import IsAdminOrReadOnlyIfAuthenticated
 from .models import Club
 from .serilaizers import ClubSerializer
 
@@ -7,7 +9,7 @@ from .serilaizers import ClubSerializer
 # Create your views here.
 
 class ClubView(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAdminOrReadOnlyIfAuthenticated]
 
     queryset = Club.objects.all()
     serializer_class = ClubSerializer
