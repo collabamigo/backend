74f8f5a923afdbd5002a6b537c9e90ee2742e263
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-08-21 09:32:15 +0530

    views , serializer first push

---
+++ club/serilaizers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+# from rest_framework.relations import PrimaryKeyRelatedField
+from .models import Club
+# Competition, Entries
+
+
+class ClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = ('id', 'name', 'link', 'picture', 'college', 'join_date')
---
+++ club/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework import routers
+from . import views
+from backend import settings
+
+if settings.DEBUG:
+    router = routers.DefaultRouter()
+else:
+    router = routers.SimpleRouter()
+
+router.register(r'club', views.ClubView, 'club')
+
+urlpatterns = [
+    path('clubdata/', views.ClubView.as_view(), name='ClubData'),
+]
+
+urlpatterns += router.urls
