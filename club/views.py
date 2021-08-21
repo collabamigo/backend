baea4b1f991112b80658447855611d3cea19b1ee
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-08-21 18:31:38 +0530

    applied permissions on ClubView

--- club/views.py
+++ club/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Club
 from .serilaizers import ClubSerializer
 
@@ -7,5 +7,7 @@ from .serilaizers import ClubSerializer
 # Create your views here.
 
 class ClubView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
     queryset = Club.objects.all()
     serializer_class = ClubSerializer
