da02c9634fcbd2a2b350eff29f6b4675aa79974f
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-08-23 23:28:32 +0530

    serializer update

--- club/serilaizers.py
+++ club/serilaizers.py
@@ -7,7 +7,7 @@ class ClubSerializer(serializers.ModelSerializer):
     class Meta:
         model = Club
         fields = ('id', 'name', 'link', 'picture', 'college', 'join_date')
-        read_only_fields = '__all__'
+        read_only_fields = ['id', 'name', 'link', 'picture', 'college', 'join_date']
 
 
 class CompetitionSerializer(serializers.ModelSerializer):
