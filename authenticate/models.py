41c4a10481a53562fbe8ab5f4a2826e8500bccc3
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-09-02 18:06:24 +0530

    bugfix

--- authenticate/models.py
+++ authenticate/models.py
@@ -6,8 +6,11 @@ from django.db import models
 from backend.settings import TOKEN_VALIDITY_IN_DAYS
 
 
+def time_delay():
+    return datetime.now() + timedelta(days=TOKEN_VALIDITY_IN_DAYS)
+
+
 class RefreshToken(models.Model):
     token = models.OneToOneField(Token, on_delete=models.CASCADE)
     expiry = \
-        models.DateTimeField(default=lambda: datetime.now() + timedelta(
-                                                 days=TOKEN_VALIDITY_IN_DAYS))
+        models.DateTimeField(default=time_delay)
