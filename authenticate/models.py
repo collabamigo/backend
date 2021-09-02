966e8527633c501a1c6005fb696e101efcf45308
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-09-02 15:34:16 +0530

    model update

--- authenticate/models.py
+++ authenticate/models.py
@@ -8,4 +8,6 @@ from backend.settings import TOKEN_VALIDITY_IN_DAYS
 
 class RefreshToken(models.Model):
     token = models.OneToOneField(Token, on_delete=models.CASCADE)
-    expiry = models.DateTimeField(default=lambda: datetime.now() + timedelta(days=TOKEN_VALIDITY_IN_DAYS))
+    expiry = \
+        models.DateTimeField(default=lambda: datetime.now() + timedelta(
+                                                 days=TOKEN_VALIDITY_IN_DAYS))
