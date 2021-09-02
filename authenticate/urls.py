920d38cfe814221153fda9fd0e2c7c1509466037
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-09-02 15:01:18 +0530

    new auth flow

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('oauthcallback/', views.OAuthCallback.as_view()),
+    path('refresh/', views.RefreshJWT.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
