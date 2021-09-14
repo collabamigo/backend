46626ac6a5b05f0c67f2f716fa35d6c869c093b9
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-09-14 10:31:44 +0530

    models ecell update

--- ecell/models.py
+++ ecell/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
+from connect.models import Profile
 
 # Create your models here.
+class eCell(models.Model):
+    id = models.AutoField(primary_key=True)
+    team_name = models.CharField(max_length=50, default='null')
+    legal_name = models.CharField(max_length=50, default='null')
+    college = models.CharField(max_length=100, default="IIIT-D")
+    join_date = models.DateField(auto_now_add=True)
