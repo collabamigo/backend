06558fe6aee8578df42bb1eed7afa0583b5e02cb
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-09-20 23:31:46 +0530

    minor update

--- authenticate/authbackend.py
+++ authenticate/authbackend.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework import authentication, permissions
@@ -13,9 +12,11 @@ from .models import RefreshToken
 
 class CustomAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request: Request):
-        if 'Authorization' in request.headers and request.headers['Authorization'].startswith("Token"):
-            jwt_payload = jwt.decode(request.headers["Authorization"].split()[1], settings.JWT_SECRET,
-                                     algorithms=["HS256"])
+        if 'Authorization' in request.headers and \
+                request.headers['Authorization'].startswith("Token"):
+            jwt_payload = jwt.decode(
+                request.headers["Authorization"].split()[1],
+                settings.JWT_SECRET, algorithms=["HS256"])
 
             user:User = User.objects.get(email=jwt_payload.get("email"))
             hasher = SHA512.new(truncate="256")
@@ -23,7 +24,8 @@ class CustomAuthentication(authentication.BaseAuthentication):
             hasher.update(token.token.key.encode('utf-8'))
 
             if jwt_payload.get(
-                    "hash") == hasher.hexdigest() and datetime.now() < datetime.fromtimestamp(jwt_payload['exp']):
+                    "hash") == hasher.hexdigest() and datetime.now()\
+                    < datetime.fromtimestamp(jwt_payload['exp']):
                 print(request.method+" request received on " +
                       request.path+" by "+jwt_payload['email'] +
                       " with data "+str(request.query_params),
@@ -38,9 +40,11 @@ class CustomAuthentication(authentication.BaseAuthentication):
 
 class DummyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request: Request):
-        if 'Authorization' in request.headers and request.headers['Authorization'] == "Token 00000.dummy.00000":
+        if 'Authorization' in request.headers and \
+                request.headers['Authorization'] == "Token 00000.dummy.00000":
             if request.method in permissions.SAFE_METHODS:
-                return User.objects.get_or_create(email="dummy.user@collabamigo.com"), None
+                return User.objects.get_or_create(
+                    email="dummy.user@collabamigo.com"), None
             else:
                 raise AuthenticationFailed()
         else:
