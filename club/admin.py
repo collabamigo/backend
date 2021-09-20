a065103c10b43b2261dc27f1353c9212a3f847b7
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-09-20 09:26:38 +0530

    minor update

--- club/admin.py
+++ club/admin.py
@@ -5,7 +5,7 @@ from .models import Club, Competition
 # Register your models here.
 
 class ClubAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "picture", "college", "join_date", "admins",
+    list_display = ("id", "name", "picture", "college", "join_date",
                     "instagram", "linkedin", "facebook", "discord", "other")
 
 
