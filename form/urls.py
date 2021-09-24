78a860adeea0d9b929bd2dc51dcd384011cbde74
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-09-25 00:06:29 +0530

    update for ecell

---
+++ form/urls.py
@@ -0,0 +1,17 @@
+# from django.urls import path
+# from rest_framework import routers
+# from . import views
+# from backend import settings
+#
+# if settings.DEVELOPMENT:
+#     router = routers.DefaultRouter()
+# else:
+#     router = routers.SimpleRouter()
+#
+# router.register(r'profile','profile')
+#
+# urlpatterns = [
+#     path('', views., name='ClubData'),
+# ]
+#
+# urlpatterns += router.urls
