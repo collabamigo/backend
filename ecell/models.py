8a6809ba2e05887757bb524bee3a25fc347b88ce
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-09-27 00:34:58 +0530

    update for ecell, minor update pending

--- ecell/models.py
+++ ecell/models.py
@@ -4,16 +4,18 @@ from django.db import models
 # Create your models here.
 class idea(models.Model):
     id = models.AutoField(primary_key=True)
-    role = models.CharField(max_length=8, choices=["ideator", "member"])
+    role = models.CharField(max_length=8, choices=[("i", "ideator"),
+                                                   ("m", "member")])
     name = models.CharField(max_length=50, default='null')
     profile = models.OneToOneField(to="Profile", on_delete=models.CASCADE,
                                    related_name='profile')
     idea = models.TextField(max_length=900)
     visibility = models.CharField(max_length=8, choices=["public", "private"])
-    stage = models.CharField(max_length=40, choices=["Initiation", "Planning",
-                                                     "Execution",
-                                                     "Monitoring and"
-                                                     " Controllling",
-                                                     "Closure"])
+    stage = models.CharField(max_length=40, choices=[("i", "Initiation"),
+                                                     ("p", "Planning"),
+                                                     ("e", "Execution"),
+                                                     ("mc", "Monitoring and"
+                                                     " Controllling"),
+                                                     ("c", "Closure")])
     college = models.CharField(max_length=100, default="IIIT-D")
     join_date = models.DateField(auto_now_add=True)
