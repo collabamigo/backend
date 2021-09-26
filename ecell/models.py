7d02213b9a714cec507949a1abdd9ba031d1554c
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-09-26 18:24:21 +0530

    Revert "update for ecell"
    This reverts commit 12d4d2150a621dd8f6b995df346476d6cd3ffdc8.

--- ecell/models.py
+++ ecell/models.py
@@ -2,20 +2,9 @@ from django.db import models
 
 
 # Create your models here.
-class Ecell(models.Model):
+class eCell(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50, default='null')
-    role = models.CharField(max_length=8, choices=["ideator", "member"])
-    batch = models.IntegerField(default="null")
-    stream = models.CharField(default="null")
-    email = models.EmailField(default="null@gmail.com")
-    mobile_number = models.BigIntegerField(default="null")
-    idea = models.TextField(max_length=900)
-    visibility = models.CharField(max_length=8, choices=["public", "private"])
-    stage = models.CharField(max_length=40, choices=["Initiation", "Planning",
-                                                     "Execution",
-                                                     "Monitoring and"
-                                                     " Controllling",
-                                                     "Closure"])
+    team_name = models.CharField(max_length=50, default='null')
+    legal_name = models.CharField(max_length=50, default='null')
     college = models.CharField(max_length=100, default="IIIT-D")
     join_date = models.DateField(auto_now_add=True)
