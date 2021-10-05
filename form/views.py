ed4c7a47eacdd25e583eb6809e6c37977314e78a
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-10-05 09:44:25 +0530

    update for views

--- form/views.py
+++ form/views.py
@@ -13,9 +13,3 @@ class FormView(viewsets.ModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
 
-    # def perform_create(self, serializer):
-    #     queryset = Form.objects.all()
-    #     if queryset.exists():
-    #         return Response("Your response is already with us!",
-    #                         status=status.HTTP_208_ALREADY_REPORTED)
-    #     serializer.save()
