ffd32f6f18ec50bef2ebeb8f3de17c88ed304de2
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-11-14 18:34:53 +0530

    update for club

--- club/serializers.py
+++ club/serializers.py
@@ -9,13 +9,15 @@ class ClubSerializer(serializers.ModelSerializer):
         model = Club
         fields = ('id', 'name', 'picture', 'college', 'join_date',
                   'instagram', 'linkedin', 'facebook', 'discord', 'other',
-                  'username', 'memberSize', 'tagline', 'description', 'announcements')
+                  'username', 'memberSize', 'tagline', 'description',
+                  'announcements')
         read_only_fields = ['id', 'name', 'picture', 'college',
                             'join_date', 'username', 'announcements']
 
 
 class CompetitionSerializer(serializers.ModelSerializer):
-    club = SlugRelatedField(allow_empty=False, many=True, queryset=Club.objects.all(), slug_field='username')
+    club = SlugRelatedField(allow_empty=False, many=True,
+                            queryset=Club.objects.all(), slug_field='username')
 
     class Meta:
         model = Competition
