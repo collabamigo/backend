5c42d0d124c106b66be6fc196c50864e2ac654b7
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-12-22 23:39:44 +0530

    bugfix

--- connect/emailhandler.py
+++ connect/emailhandler.py
@@ -1,7 +1,6 @@
 import os
 from django.core import mail
 
-from backend import settings
 
 EMAIL_HOST_USER = "CollabConnect <" + os.getenv("EMAIL") + ">"
 
@@ -10,12 +9,6 @@ def send_mail(to, subject: str = "", body: list = None, html=None):
     if type(to) == str:
         to = [to]
     print("Sending mail to " + ", ".join(to), flush=True)
-    if not settings.DEVELOPMENT or set(settings.ALLOWED_IN_DEBUG) >= set(to):
-        mail.send_mail(subject=subject, message=body,
-                       from_email=EMAIL_HOST_USER, recipient_list=to,
-                       html_message=html)
-    else:
-        print("Attempt to send email to "
-              "non-whitelisted address in DEVELOPMENT mode", flush=True)
-        raise ConnectionRefusedError("Attempt to send email to "
-                                     "non-whitelisted address in DEVELOPMENT mode")
+    mail.send_mail(subject=subject, message=body,
+                   from_email=EMAIL_HOST_USER, recipient_list=to,
+                   html_message=html)
