572bf58823919d34897fcfdda0378ad207f0a1fa
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-12-25 23:36:52 +0530

    cors allowed_hosts bugfix

--- backend/settings.py
+++ backend/settings.py
@@ -22,8 +22,8 @@ load_dotenv()
 
 env = environ.Env(
     DATABASE_URL=(str, ""),
-    CORS_ORIGIN_WHITELIST=(list, ["http://localhost:3000", "https://collabamigo.xyz"]),
-    ALLOWED_HOSTS=(list, ["localhost", "blooming-peak-53825.herokuapp.com"]),
+    CORS_ORIGIN_WHITELIST=(str, '["http://localhost:3000", "https://collabamigo.xyz"]'),
+    ALLOWED_HOSTS=(str, '["localhost", "blooming-peak-53825.herokuapp.com"]'),
 )
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': [
         'authenticate.authentication.CustomAuthentication']}
 
 
-CORS_ORIGIN_WHITELIST = env("CORS_ORIGIN_WHITELIST")
+CORS_ORIGIN_WHITELIST = json.loads(env("CORS_ORIGIN_WHITELIST"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -48,7 +48,7 @@ CORS_ORIGIN_WHITELIST = env("CORS_ORIGIN_WHITELIST")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "TESTSEcret" if DEVELOPMENT else os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+ALLOWED_HOSTS = json.loads(env("ALLOWED_HOSTS"))
 
 # DataFlair neeche hai
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
