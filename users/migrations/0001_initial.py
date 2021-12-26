493b388130481a57aa2737618a9744dc338effc1
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-12-26 23:25:51 +0530

    updates

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = [('auth', '0012_alter_user_first_name_max_length'),
+                    ]
 
     operations = [
         migrations.CreateModel(
