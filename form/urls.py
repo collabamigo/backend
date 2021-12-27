d613ad98b1ff2f3f1ecbb475fe5117e4b3b74ee3
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-12-27 16:58:31 +0530

    new endpoint

--- form/urls.py
+++ form/urls.py
@@ -1,4 +1,3 @@
-# from django.urls import path
 from django.urls import path, re_path
 from rest_framework import routers
 from . import views
@@ -14,6 +13,8 @@ router.register(r'form', views.FormView, 'form')
 urlpatterns = [
     path('submit/<int:event_id>/', views.SubmitResponseView.as_view(), name='submit_response'),
     re_path('^response/(?P<competition_id>.+)/$', views.ResponseDisplayView.as_view(), name='response_display'),
+    re_path('^get-response/(?P<competition_id>.+)/$', views.SelfFormResponseAPIView.as_view(),
+            name='self_response_display'),
     path('participation-history/', views.ParticipationHistoryView.as_view(), name='participation_history'),
 ]
 
