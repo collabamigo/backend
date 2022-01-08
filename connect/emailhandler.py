b169bf267eabbb6ed5c8b7227e24db2318e9cbd0
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-01-08 19:37:15 +0530

    disabled email

--- connect/emailhandler.py
+++ connect/emailhandler.py
@@ -1,14 +1,15 @@
 import os
-from django.core import mail
+# from django.core import mail
 
 
 EMAIL_HOST_USER = "CollabConnect <" + os.getenv("EMAIL") + ">"
 
 
 def send_mail(to, subject: str = "", body: list = None, html=None):
-    if type(to) == str:
-        to = [to]
-    print("Sending mail to " + ", ".join(to), flush=True)
-    mail.send_mail(subject=subject, message=body,
-                   from_email=EMAIL_HOST_USER, recipient_list=to,
-                   html_message=html)
+    return
+    # if type(to) == str:
+    #     to = [to]
+    # print("Sending mail to " + ", ".join(to), flush=True)
+    # mail.send_mail(subject=subject, message=body,
+    #                from_email=EMAIL_HOST_USER, recipient_list=to,
+    #                html_message=html)
