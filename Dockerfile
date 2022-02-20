c9de48e0cd23e6cf5c193a6e30242e02b1e3561f
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-02-21 01:23:34 +0530

    Revert "docker files"
    This reverts commit da86d156a13d800c73b270e3a90cd90866367ab7.

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,15 @@
 FROM python:3.9.2
 ENV PYTHONUNBUFFERED 1
 ENV PYTHONDONTWRITEBYTECODE 1
-#WORKDIR /app
-ENV PYTHONPATH /opt/deploy/bezen
+WORKDIR /app
 
 
-COPY requirements.txt $PYTHONPATH/requirements.txt
-WORKDIR $PYTHONPATH
-RUN pip3 install --no-cache-dir --compile -r requirements.txt
-COPY . $PYTHONPATH
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+COPY . .
 RUN chmod +x ./utils/run.sh
 RUN ln -s /run/shm /dev/shm
 
-ENV HOME ${PYTHONPATH}
 
 EXPOSE 80
 ENTRYPOINT ["utils/run.sh"]
--- utils/run.sh
+++ utils/run.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
-
 set -e
-
 : ${PORT:=80}
 
-until cd /opt/deploy/collabamigo
-do
-    echo "Waiting for server volume..."
-done
-
-# Apply database migrations
-echo "Waiting for db to be ready...."
-#python manage.py makemigrations
-if [[ $DEBUG == "True" ]] && [[ $DJANGO_TEST_SERVER == "True" ]];
-then
-    echo "Skipping migrate and collectstatic"
-else
-    python manage.py migrate
-    python manage.py collectstatic --noinput
-fi
-# Collect static files
-
-
-echo "------------------------------"
-echo "DEBUG: " "$DEBUG"
-echo "DJANGO_TEST_SERVER: " "$DJANGO_TEST_SERVER"
-echo "------------------------------"
-
-if [[ $DEBUG == "True" ]] && [[ $DJANGO_TEST_SERVER == "True" ]];
-then
-    python manage.py runserver 0.0.0.0:80
-else
-    gunicorn backend.wsgi:application --bind 0.0.0.0:"$PORT"
-fi
+python ./manage.py migrate --noinput
+gunicorn backend.wsgi:application --bind 0.0.0.0:"$PORT"
