81a1d949aade2d95c2a731bfef96d80c46941eee
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-02-22 00:42:30 +0530

    cleanup

--- backend/settings.py
+++ backend/settings.py
@@ -228,24 +228,3 @@ if os.getenv("FIREBASE_CREDENTIALS"):
     firebase_app = firebase_admin.initialize_app(firebase_cred)
 else:
     firebase_app = None
-
-import socket
-import struct
-import sys
-import time
-import datetime
-
-def RequestTimefromNtp(addr='0.de.pool.ntp.org'):
-    REF_TIME_1970 = 2208988800  # Reference time
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    data = b'\x1b' + 47 * b'\0'
-    client.sendto(data, (addr, 123))
-    data, address = client.recvfrom(1024)
-    if data:
-        t = struct.unpack('!12I', data)[10]
-        t -= REF_TIME_1970
-    return time.ctime(t), t
-
-
-print(RequestTimefromNtp())
-print(datetime.datetime.now())
